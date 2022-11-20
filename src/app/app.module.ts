5d38b108b2c4113fb51540c7496b9c936b9a6ef7
Samarpit <verma.samarpit@gmail.com>
2022-11-21 00:07:09 +0530

    homepage carousel done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { BodyComponent } from './layout/body/body.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BodyComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
